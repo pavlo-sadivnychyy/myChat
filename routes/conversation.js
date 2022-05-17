057e33e5e8eb011ee049979f9c827e6b6731f3d3
pavlo1998 <pavlo.sad1@gmail.com>
2022-05-17 11:56:04 +0300

    css fix

--- routes/conversation.js
+++ routes/conversation.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+ const router = require('express').Router();
 const Conversation = require('../models/Conversation')
 const mongoose = require('mongoose');
 
